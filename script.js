ad2a5ac0cc241218ad00b09a91e2a99cf08368c2
codergirl20 <ks4264703@gmail.com>
2023-08-05 18:32:00 +0530

    i have made some changes to improve my codes

---
+++ script.js
@@ -0,0 +1,8 @@
+const qualadd = () => {
+    var qcs = document.getElementById("qc").innerHTML
+    document.getElementById("qc").innerHTML= qcs + '<input type="text" placeholder="qualifications">'
+}
+const weadd = () => {
+    var wes=document.getElementById("we").innerHTML
+    document.getElementById("we").innerHTML=wes+'<input type="text" placeholder="Work experiences">'
+}
